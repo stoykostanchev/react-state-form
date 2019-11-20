e0ac14da07f36367eac8aa798b88080463f411cd
Stoyko Stanchev <stoyko.stanchev@gomedia.io>
2019-11-20 01:50:42 +0000

    Move code around to keep tests happy

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
 import PersonForm from './person/Form';
+import { applyMiddleware, createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+import thunk from 'redux-thunk';
 import './App.css';
 
 function App() {
+  const store = createStore(reducer, applyMiddleware(thunk))
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <PersonForm></PersonForm>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <PersonForm></PersonForm>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </Provider>
   );
 }
 
